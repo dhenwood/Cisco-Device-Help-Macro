b3e0773b92118b57d2897d631f8d62f7d397c222
dhenwood <dhenwood@users.noreply.github.com>
2024-05-25 16:08:12 +1000

    Create HelpVideoLibrary.js

---
+++ HelpVideoLibrary.js
@@ -0,0 +1,44 @@
+
+// Desk Series URLS
+function deskSeries(){
+  const deskSeriesUrls = [
+    "https://www.youtube.com/watch?v=GyXu1qQ8NsI&autoplay=1", // Webex Help
+    "https://www.youtube.com/watch?v=TJkz7oxIrOw&autoplay=1", // Microsoft Help
+    "https://www.youtube.com/watch?v=b1wEz0gYDeI&autoplay=1", // Zoom Help
+    "https://www.youtube.com/watch?v=8JX-_FxsO8g&autoplay=1", // Google Help
+    "https://www.youtube.com/watch?v=GyXu1qQ8NsI&autoplay=1", // Wired Share
+    "https://www.youtube.com/watch?v=u4fv9qqL37U&autoplay=1" // Wireless (airplay) Share
+  ]
+  return deskSeriesUrls
+}
+
+// Board Series URLS
+function boardSeries(){
+  const boardSeriesUrls = [
+    "https://www.youtube.com/watch?v=GyXu1qQ8NsI&autoplay=1", // Webex Help
+    "https://www.youtube.com/watch?v=TJkz7oxIrOw&autoplay=1", // Microsoft Help
+    "https://www.youtube.com/watch?v=b1wEz0gYDeI&autoplay=1", // Zoom Help
+    "https://www.youtube.com/watch?v=8JX-_FxsO8g&autoplay=1", // Google Help
+    "https://www.youtube.com/watch?v=GyXu1qQ8NsI&autoplay=1", // Wired Share
+    "https://www.youtube.com/watch?v=u4fv9qqL37U&autoplay=1" // Wireless (airplay) Share
+  ]
+  return boardSeriesUrls
+}
+
+// Room Series URLS
+function roomSeries(){
+  const roomSeriesUrls = [
+    "https://www.youtube.com/watch?v=GyXu1qQ8NsI&autoplay=1", // Webex Help
+    "https://www.youtube.com/watch?v=TJkz7oxIrOw&autoplay=1", // Microsoft Help
+    "https://www.youtube.com/watch?v=b1wEz0gYDeI&autoplay=1", // Zoom Help
+    "https://www.youtube.com/watch?v=8JX-_FxsO8g&autoplay=1", // Google Help
+    "https://www.youtube.com/watch?v=GyXu1qQ8NsI&autoplay=1", // Wired Share
+    "https://www.youtube.com/watch?v=u4fv9qqL37U&autoplay=1" // Wireless (airplay) Share
+  ]
+  return roomSeriesUrls
+}
+
+
+export {deskSeries};
+export {boardSeries}
+export {roomSeries}
